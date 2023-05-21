5b92314cfa67b7c0186f24c1b64e688ab136b622
Luka Grdinic <1lukagrdinic@gmail.com>
2023-05-21 22:07:43 +0100

    Initial commit

---
+++ apps/h-tags-playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-dynamic-headings-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'h-tags-playground';
+}
