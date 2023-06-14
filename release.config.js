aed57f3e4dcd146e2c8bb21b546e2b9ec6ef81ef
Luka Grdinic <1lukagrdinic@gmail.com>
2023-06-14 08:21:39 +0100

    ci: add push workflow

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    branches: 'main',
+    repositoryUrl: 'https://github.com/LukaGrdinic/ngx-dynamic-headings',
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/github',
+        {
+          assets: [
+            { path: 'build.zip', label: 'Build' },
+            { path: 'coverage.zip', label: 'Coverage' },
+          ],
+        },
+      ],
+      ["@semantic-release/npm", {
+        "pkgRoot": "/libs/ngx-dynamic-headings"
+      }]
+    ],
+  };
