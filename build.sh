a121cbf42483841cbfb8d2591a61449c00c91a64
LukaGrdinic2 <2lukagrdinic@gmail.com>
2023-07-18 12:16:28 +0100

    fix: set git credentials in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Setup git credentials
+git config --global credential.helper 'store --file ~/.git-credentials'
+echo "https://github.com:LukaGrdinic2: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" >> ~/.git-credentials
+chmod 600 ~/.git-credentials
+
 # Build library with npm script
 npm run build:ngx-dynamic-headings
+
