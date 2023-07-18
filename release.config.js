2c6fae94e1e6e566bd6aeac9455eb11fb93ffd4b
LukaGrdinic2 <2lukagrdinic@gmail.com>
2023-07-18 09:42:23 +0100

    fix: remove unneeded steps in push workflow

--- release.config.js
+++ release.config.js
@@ -3,7 +3,6 @@ module.exports = {
     repositoryUrl: 'https://github.com/LukaGrdinic2/ngx-dynamic-headings-cicd-setup',
     plugins: [
       '@semantic-release/commit-analyzer',
-      // release-notes-generator and @semantic-release/github should execute after @semantic-release/exec (after the build)
       '@semantic-release/release-notes-generator',
       '@semantic-release/github',
       ["@semantic-release/npm", {
